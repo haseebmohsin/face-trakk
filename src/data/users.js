f546ee306785c2b604c5e38297bff942b2a56087
haseebmohsin <hasib.muhsan@gmail.com>
2023-08-07 15:58:00 +0500

    ahmed flow begins

--- src/data/users.js
+++ src/data/users.js
@@ -2,13 +2,7 @@ export const users = [
   {
     id: 1,
     username: 'ali',
-    email: '1@gmail.com',
-    password: '$2a$12$3dNTqC2JMrit3nzl3YYZUeVsq9lHZTUM9BGlN8NhIyZNDN7Z53VbS',
-  },
-  {
-    id: 2,
-    username: 'Haseeb',
-    email: 'haseeb11634@gmail.com',
-    password: '$2a$12$ia1Zi/rA39vrwWX/XhBVo.Jr6C4pefEHYMMeOwLkhegvZN1et.gje',
+    email: 'admin@admin.com',
+    password: '$2a$12$0y6UyamgnDvwQuoS7nwkG.Sg1wmvME7etrk4YGvZ6toohi09CHYpC',
   },
 ];
